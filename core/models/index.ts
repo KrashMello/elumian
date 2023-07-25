1b3ec54530dde6fdab776da9d4a6e95d37a62343
KrashMello <joelenrikesoteldo@gmail.com>
2023-07-25 15:48:29 -0400

    update create schema

--- core/models/index.ts
+++ core/models/index.ts
@@ -1,5 +1,17 @@
+import { DB as db } from '@DB/index'
 export abstract class Model {
   protected tableName: string = 'test'
   public DB: any
-  constructor() {}
+  constructor() {
+    this.DB = new db(this.tableName)
+  }
+  protected async findAll() {
+    let { rows } = await this.DB.select().exec()
+    return rows
+  }
+  protected async findOne(id: string) {
+    let { rows } = await this.DB.select().where(`"id" = '${id}'::bigint`).exec()
+
+    return rows[0]
+  }
 }
