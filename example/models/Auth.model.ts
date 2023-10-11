4947868b86a144755f1694f26654066855965f70
KrashMello <joelenrikesoteldo@gmail.com>
2023-10-11 13:28:34 -0400

    fix: Auth model

--- example/models/Auth.model.ts
+++ example/models/Auth.model.ts
@@ -4,26 +4,33 @@ import bcrypt from 'bcrypt'
 import { type Response } from 'express'
 
 export class AuthModel extends Model {
-  tableName = 'User'
   private readonly cacheKey: string = 'Auth'
+  schemaName = 'system'
+  tableName = 'Users'
+  constructor() {
+    super()
+    this.initBD()
+  }
 
   public signIn(
     data: { username: string, password: string },
     resp: Response
   ): any {
     const { username, password } = data
-    const fields = ['username', 'first_name', 'last_name', 'password']
-    const userData = this.DB.view(fields, 'user_view')
+    const fields = ['username', 'first_name', '"last_name"', 'password']
+    let userData: any
+    this.DB.view(fields, 'user_view')
       .where([`"username" = '${username}'`])
       .exec().then((res: any) => {
-        if (res.row.lenght === 0)
+        if (res.rowCount === 0)
           return resp.status(401).json({ message: "usuario invalido" })
+        userData = res.rows[0]
         if (!bcrypt.compareSync(password, userData.password as string)) {
           return resp.status(401).json({
             message: 'contraseña invalida'
           })
         }
-        return res.status(200).json(singCacheData(this.cacheKey, userData))
+        return resp.status(200).json(singCacheData(this.cacheKey, userData))
       }).catch((error: any) => {
         console.log(error)
         return resp.status(401).json({ message: "ha ocurrido un error!" })
@@ -45,7 +52,7 @@ export class AuthModel extends Model {
     },
     resp: Response
   ): any {
-    this.DB.call('createuser', `'${data.username}'::varchar , '${bcrypt.hashSync(data.password, 10)}'::varchar, '${data.email}'::varchar, '${data.first_name}'::varchar, '${data.last_name}'::varchar`).exec().then((res: any) => {
+    this.DB.call('createuser', `'${data.username}'::varchar , '${bcrypt.hashSync(data.password, 10)}'::varchar, '${data.email}'::varchar, '${data.first_name}'::varchar, '${data.last_name}'::varchar`).exec().then((_res: any) => {
       return resp.status(200).json({ message: "Usuario creado" });
     }).catch((error: any) => {
       console.log(error)
