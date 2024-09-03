110496d697a74289c413179778df270924b009d3
KrashMello <joelenrikesoteldo@gmail.com>
2024-09-03 15:38:17 -0400

    feat: package json build script and types in server router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,19 +2,8 @@ import type { SRouter, IRouter } from "elumian/core/decorators";
 import type { NextFunction, Request, Response } from "express";
 import "reflect-metadata";
 import { getIPV4 } from "../server";
-export interface RouteInfo {
-  api: string;
-  handler: string;
-  protected: boolean;
-}
+import { RouteInfo, type SocketInfo, SocketRoute } from "./type";
 
-export interface SocketRoute {
-  method: string;
-  path: string;
-  functionSocket: (io: any, socket: any) => void;
-}
-
-export type SocketInfo = { method: string; path: string; handlerName: string };
 const IPV4 = getIPV4();
 const DEFAULT_PORT = process.env.PORT ?? 5000;
 
