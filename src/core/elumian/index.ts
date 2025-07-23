ee5ec4678c9f2903a1636e1977a8a0ee1770cfcd
KrashMello <joelenrikesoteldo@gmail.com>
2025-07-23 18:19:20 -0400

    feat: add prisma to elumian again

--- src/core/elumian/index.ts
+++ src/core/elumian/index.ts
@@ -1,8 +1,11 @@
 import Cache from "../cache";
 import Crypto from "../crypto";
 import { elumian } from "../type";
+//@ts-ignore
+import { PrismaClient } from "@prisma/client";
 
 export const Elumian: elumian = {
+  prisma: new PrismaClient(),
   cache: Cache,
   crypto: Crypto,
 };
