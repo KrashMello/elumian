72ed6d62379a9c46036d57e798bf8d9c3042d262
KrashMello <joelenrikesoteldo@gmail.com>
2025-08-15 09:21:47 -0400

    feat(test/crypto): add new test

--- test/crypto/index.test.ts
+++ test/crypto/index.test.ts
@@ -1,6 +1,10 @@
-import eln from "elumian/core/crypto"
-let time = Math.floor(Math.random() * 10) + 1
-console.log(time)
-let encrypted = eln.hardEncryptor({ id: 1, name: 'asd' }, time)
-console.log(encrypted)
-console.log(eln.hardDecrypt(encrypted))
+import { Elumian } from "elumian/core";
+let encrypted = Elumian.crypto.hardEncrypt({ id: 1, name: "asd" });
+console.log(encrypted);
+console.log(Elumian.crypto.hardDecrypt(encrypted));
+console.log(
+	Elumian.crypto.hardDecrypt(
+		"cgtzG1lTxwn8Ha.ijxJ5Ohx6sL1/DdAReuWB/1u264zhb/iBYoVoTdrwHDINWBZKEKrCwyzEC/XL9RZO7xVcxHuy2HQOpaAWONaZUA1B33dmTeU2fqsJw8snmE=",
+		true,
+	),
+);
