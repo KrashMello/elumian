a8fd40b2692c252ef22038bf0d10def2052ebad3
KrashMello <joelenrikesoteldo@gmail.com>
2025-08-15 09:19:53 -0400

    feat(core/elumian): drop prisma dependencie

--- src/core/elumian/index.ts
+++ src/core/elumian/index.ts
@@ -1,11 +1,8 @@
 import * as Cache from "../cache";
 import * as Crypto from "../crypto";
 import { elumian } from "../type";
-//@ts-ignore
-import { PrismaClient } from "@prisma/client";
 
 export const Elumian: elumian = {
-	prisma: new PrismaClient(),
 	cache: Cache,
 	crypto: Crypto,
 };
